58cde4e1cd2bde3082b14ab5313242b68fe17bcb
liyang <flippia0408@gmail.com>
2023-04-02 15:03:52 -0400

    hash route using a href added to page 3

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -1,23 +1,27 @@
 import styles from "./Breadcrumbs.module.css";
 import { useLocation, Link } from "react-router-dom";
 
-export default function Breadcrumbs() {
+const Breadcrumbs = () => {
   const location = useLocation();
+
+  //   console.log("location from useLocation", location);
 
   let currentLink = "";
 
   const crumbs = location.pathname
     .split("/")
     .filter((crumb) => crumb !== "")
-    .map((crumb) => {
+    .map((crumb, index) => {
       currentLink += `/${crumb}`;
 
       return (
-        <div className={styles.crumb} key={crumb}>
+        <div className={styles.crumb} key={index}>
           <Link to={currentLink}>{crumb}</Link>
         </div>
       );
     });
 
   return <div className={styles.breadcrumbs}>{crumbs}</div>;
-}
+};
+
+export default Breadcrumbs;
--- src/pages/Page3.jsx
+++ src/pages/Page3.jsx
@@ -1,7 +1,27 @@
+import { Button } from "@mui/material";
+
 const Page3 = () => {
   return (
     <div>
-      <h1>Page3</h1>
+      <h1>Hash Route</h1>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <a href="#page-3-section-1">
+          <Button variant="outlined" color="warning">
+            Section 1
+          </Button>
+        </a>
+        <a href="#page-3-section-2">
+          <Button variant="outlined" color="warning">
+            Section 2
+          </Button>
+        </a>
+        <a href="#page-3-section-3">
+          <Button variant="outlined" color="warning">
+            Section 3
+          </Button>
+        </a>
+      </div>
+      <h2 id="page-3-section-1">Section1</h2>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, nemo
         fugit non accusantium quaerat accusamus veniam. Sint dolore voluptas
@@ -40,6 +60,106 @@ const Page3 = () => {
         distinctio necessitatibus quisquam voluptate labore. Quidem et ea illum
         esse unde perspiciatis, impedit nemo. Dolorum labore dolore similique ab
         deserunt nihil voluptas repellat facere, ipsam atque, voluptatem magnam.
+      </p>
+      <h2 id="page-3-section-2">Section2</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, nemo
+        fugit non accusantium quaerat accusamus veniam. Sint dolore voluptas
+        nemo architecto voluptate nihil corporis laborum ratione. Nam
+        necessitatibus exercitationem ratione odio nisi officiis facere vitae ex
+        consequuntur quam laborum excepturi tempora eos expedita eius voluptate
+        numquam temporibus nulla vero accusamus ab, aut doloremque doloribus
+        deserunt! Sint animi rem aliquam labore, dicta temporibus officia
+        obcaecati, ipsum magnam natus consequuntur nisi beatae quisquam. Vel
+        distinctio necessitatibus quisquam voluptate labore. Quidem et ea illum
+        esse unde perspiciatis, impedit nemo. Dolorum labore dolore similique ab
+        deserunt nihil voluptas repellat facere, ipsam atque, voluptatem magnam.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda
+        nesciunt voluptatum aperiam ipsa, ullam deserunt dolore quia
+        perspiciatis, ratione doloremque quod earum labore itaque omnis illum
+        laborum numquam sapiente voluptatem? Provident, perspiciatis. Cupiditate
+        odit necessitatibus, iste repellendus repellat fuga perferendis quidem
+        praesentium minus laudantium explicabo fugiat omnis, maiores blanditiis
+        enim nisi quisquam aperiam officiis ipsum sunt quos. Repudiandae id
+        ratione pariatur non eum dolorum, illum eaque, blanditiis aut explicabo
+        minus cumque autem nulla harum odio ab quis. Eligendi dolor fugiat
+        provident sequi, repellendus et ducimus nisi autem eum non sunt illo
+        ipsam nihil. Laboriosam reiciendis accusamus commodi, esse vel dolores
+        nihil eaque reprehenderit officiis dolorem porro voluptates atque animi
+        consequuntur nemo mollitia sunt aperiam provident! Consectetur eos
+        dolores voluptatibus nesciunt, eligendi quisquam minus consequuntur
+        perferendis saepe ea! Rerum excepturi eveniet, qui voluptatibus sint
+        nulla fugiat beatae id quam aspernatur rem consectetur iusto autem animi
+        blanditiis quisquam perferendis tempora facere velit ipsa? Quisquam,
+        reprehenderit laboriosam, qui amet molestiae mollitia expedita dolorum,
+        ab rerum id vero voluptas doloremque ipsam autem dolorem molestias
+        consequatur nihil placeat veniam excepturi assumenda. Sunt,
+        consequuntur, aliquid voluptatem odio repellat sequi atque dolore ea
+        odit accusamus repudiandae aspernatur eligendi similique impedit
+        pariatur nulla minus iure fuga dignissimos sint!
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda
+        nesciunt voluptatum aperiam ipsa, ullam deserunt dolore quia
+        perspiciatis, ratione doloremque quod earum labore itaque omnis illum
+        laborum numquam sapiente voluptatem? Provident, perspiciatis. Cupiditate
+        odit necessitatibus, iste repellendus repellat fuga perferendis quidem
+        praesentium minus laudantium explicabo fugiat omnis, maiores blanditiis
+        enim nisi quisquam aperiam officiis ipsum sunt quos. Repudiandae id
+        ratione pariatur non eum dolorum, illum eaque, blanditiis aut explicabo
+        minus cumque autem nulla harum odio ab quis. Eligendi dolor fugiat
+        provident sequi, repellendus et ducimus nisi autem eum non sunt illo
+        ipsam nihil. Laboriosam reiciendis accusamus commodi, esse vel dolores
+        nihil eaque reprehenderit officiis dolorem porro voluptates atque animi
+        consequuntur nemo mollitia sunt aperiam provident! Consectetur eos
+        dolores voluptatibus nesciunt, eligendi quisquam minus consequuntur
+        perferendis saepe ea! Rerum excepturi eveniet, qui voluptatibus sint
+        nulla fugiat beatae id quam aspernatur rem consectetur iusto autem animi
+        blanditiis quisquam perferendis tempora facere velit ipsa? Quisquam,
+        reprehenderit laboriosam, qui amet molestiae mollitia expedita dolorum,
+        ab rerum id vero voluptas doloremque ipsam autem dolorem molestias
+        consequatur nihil placeat veniam excepturi assumenda. Sunt,
+        consequuntur, aliquid voluptatem odio repellat sequi atque dolore ea
+        odit accusamus repudiandae aspernatur eligendi similique impedit
+        pariatur nulla minus iure fuga dignissimos sint!
+      </p>
+      <h2 id="page-3-section-3">Section3</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio adipisci
+        consectetur a vero enim aspernatur sunt quisquam deserunt esse
+        voluptatem impedit in optio quae qui ex fuga, magni, harum eos nostrum
+        dolor vel alias. Tempore obcaecati nisi a ducimus facere odio facilis
+        nostrum eius animi quasi non rem, accusantium, voluptatum reprehenderit
+        cum itaque nam tempora totam fugit. Fugit ea repudiandae nam! Illum
+        numquam nisi, iste, enim, animi itaque tempora sint doloribus laboriosam
+        necessitatibus adipisci sed culpa hic vero nihil quam facere! Porro
+        nulla veritatis consectetur aspernatur vero deleniti atque
+        necessitatibus sequi fuga debitis reprehenderit, at eaque iste saepe aut
+        facere illum corrupti omnis nesciunt nostrum quisquam. Officiis pariatur
+        illum explicabo provident doloremque culpa beatae atque fuga
+        consequuntur quam totam laboriosam quibusdam libero, enim obcaecati
+        saepe voluptate? Aliquid sed iure necessitatibus maiores odio nemo
+        fugiat deleniti voluptatem maxime porro rerum, harum corporis suscipit
+        quam cupiditate quasi facere quia a! Consectetur quod deserunt molestias
+        dolor debitis, blanditiis assumenda facere aliquam numquam, veritatis
+        ab. Mollitia sint, illo temporibus facilis officia voluptates,
+        accusantium nesciunt tempore eos rem soluta deserunt doloremque
+        voluptate minus aliquid ipsum excepturi aliquam tenetur magnam
+        doloribus? Quas, eligendi eveniet. Nisi dicta facilis nostrum quod
+        eveniet numquam expedita? Autem id temporibus, ducimus a consectetur
+        veniam sequi inventore dolorem vitae explicabo mollitia cumque esse
+        optio error. Ipsa aspernatur ad unde vel facere? Asperiores pariatur,
+        porro commodi quia libero omnis esse at velit adipisci! Eius ratione
+        molestias voluptas similique in, tempore culpa adipisci omnis non libero
+        minus sit et ipsum at, voluptatibus reiciendis saepe placeat provident
+        dignissimos porro? Mollitia dolorum culpa autem aut, dolores consequatur
+        possimus, atque ullam voluptatibus facere necessitatibus rerum illum in
+        ipsum molestiae laudantium, temporibus quas quae magni. Ad, dolore
+        voluptatibus voluptatum obcaecati totam commodi quod corporis aliquid
+        assumenda impedit quos eum laborum, soluta neque maiores accusamus
+        ratione sint eos? Nostrum.
       </p>
     </div>
   );
